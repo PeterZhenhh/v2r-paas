d5f0c33c9ac2765daf310ab219c1a6f50d14a19e
PeterZhenhh <2306289410@qq.com>
2022-05-04 01:58:15 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
-# V2Ray generate configuration
-# Download and install V2Ray
+# XRay generate configuration
+# Download and install XRay
 config_path="ws_tls.json"
-mkdir /tmp/v2ray
-curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
-install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
-install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+mkdir /tmp/xray
+curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+unzip /tmp/xray/xray.zip -d /tmp/xray
+install -m 755 /tmp/xray/xray /usr/local/bin/xray
+install -m 755 /tmp/xray/geosite.dat /usr/local/bin/geosite.dat
+install -m 755 /tmp/xray/geoip.dat /usr/local/bin/geoip.dat
+xray -version
 # Remove temporary directory
-rm -rf /tmp/v2ray
-# V2Ray new configuration
-install -d /usr/local/etc/v2ray
-envsubst '\$UUID,\$WS_PATH' < $config_path > /usr/local/etc/v2ray/config.json
+rm -rf /tmp/xray
+# XRay new configuration
+install -d /usr/local/etc/xray
+envsubst '\$UUID,\$WS_PATH' < $config_path > /usr/local/etc/xray/config.json
 # MK TEST FILES
 mkdir /opt/test
 cd /opt/test
 dd if=/dev/zero of=100mb.bin bs=100M count=1
 dd if=/dev/zero of=10mb.bin bs=10M count=1
-# Run V2Ray
-/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json &
+# Run XRay
+/usr/local/bin/xray -config /usr/local/etc/xray/config.json &
 # Run nginx
 /bin/bash -c "envsubst '\$PORT,\$WS_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'

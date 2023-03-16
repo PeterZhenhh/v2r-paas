3a6a297833ae3b2d20c72411faa0cc33edce8707
PeterZhenhh <2306289410@qq.com>
2023-03-17 04:41:32 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -26,4 +26,4 @@ dd if=/dev/zero of=10mb.bin bs=10M count=1
 # Run XRay
 /usr/local/bin/xray -config /usr/local/etc/xray/config.json &
 # Run nginx
-/bin/bash -c "envsubst '\$PORT,\$WS_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
+/bin/bash -c "envsubst '\$PORT,\$UUID,\$WS_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'

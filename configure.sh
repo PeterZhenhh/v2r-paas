25d51fc6062e0f0a33b839f92bf9f09c366becad
PeterZhenhh <2306289410@qq.com>
2023-08-20 15:33:25 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -16,11 +16,11 @@ RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
 mv /tmp/xray/xray ${RELEASE_RANDOMNESS}
 envsubst '\$UUID,\$CFKEY,\$CFV6,\$CFR1,\$CFR2,\$CFR3,\$WS_PATH' < $config_path > /tmp/xray/config.json
 envsubst '\$PORT,\$UUID,\$WS_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-cat config.json | base64 > config
+cat /tmp/xray/config.json | base64 > config
 rm -f config.json
 nginx
-base64 -d config > config.json
-./${RELEASE_RANDOMNESS} -config=config.json
+base64 -d config > /tmp/xray/config.json
+./${RELEASE_RANDOMNESS} -config=/tmp/xray/config.json
 
 # MK TEST FILES
 mkdir /opt/test

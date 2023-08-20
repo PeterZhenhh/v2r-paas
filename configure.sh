501fcf153ba00b13fcba33853b2f2da7afdd431d
PeterZhenhh <2306289410@qq.com>
2023-08-20 15:31:14 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -5,27 +5,25 @@ curl -sLo warp-reg https://github.com/badafans/warp-reg/releases/download/v1.0/m
 config_path="ws_tls.json"
 mkdir /tmp/xray
 curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
-# curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/download/v1.8.1/Xray-linux-64.zip
 unzip /tmp/xray/xray.zip -d /tmp/xray
 rm /tmp/xray/geoip.dat
 rm /tmp/xray/geosite.dat
 curl -L -H "Cache-Control: no-cache" -o /tmp/xray/geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 curl -L -H "Cache-Control: no-cache" -o /tmp/xray/geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
-install -m 755 /tmp/xray/xray /usr/local/bin/xray
-install -m 755 /tmp/xray/geosite.dat /usr/local/bin/geosite.dat
-install -m 755 /tmp/xray/geoip.dat /usr/local/bin/geoip.dat
-xray -version
-# Remove temporary directory
-rm -rf /tmp/xray
-# XRay new configuration
-install -d /usr/local/etc/xray
-envsubst '\$UUID,\$CFKEY,\$CFV6,\$CFR1,\$CFR2,\$CFR3,\$WS_PATH' < $config_path > /usr/local/etc/xray/config.json
+
+# 伪装 xray 执行文件
+RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
+mv /tmp/xray/xray ${RELEASE_RANDOMNESS}
+envsubst '\$UUID,\$CFKEY,\$CFV6,\$CFR1,\$CFR2,\$CFR3,\$WS_PATH' < $config_path > /tmp/xray/config.json
+envsubst '\$PORT,\$UUID,\$WS_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+cat config.json | base64 > config
+rm -f config.json
+nginx
+base64 -d config > config.json
+./${RELEASE_RANDOMNESS} -config=config.json
+
 # MK TEST FILES
 mkdir /opt/test
 cd /opt/test
 dd if=/dev/zero of=100mb.bin bs=100M count=1
 dd if=/dev/zero of=10mb.bin bs=10M count=1
-# Run XRay
-/usr/local/bin/xray -config /usr/local/etc/xray/config.json &
-# Run nginx
-sh -c "envsubst '\$PORT,\$UUID,\$WS_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'

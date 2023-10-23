adec55f14aa3d5053051b0e01cb3a23492617b06
PeterZhenhh <2306289410@qq.com>
2023-10-23 15:58:57 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -6,10 +6,6 @@ config_path="ws_tls.json"
 mkdir /tmp/xray
 curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
 unzip /tmp/xray/xray.zip -d /tmp/xray
-rm /tmp/xray/geoip.dat
-rm /tmp/xray/geosite.dat
-curl -L -H "Cache-Control: no-cache" -o /tmp/xray/geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
-curl -L -H "Cache-Control: no-cache" -o /tmp/xray/geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 
 # 伪装 xray 执行文件
 RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
@@ -19,14 +15,12 @@ envsubst '\$PORT,\$UUID,\$WS_PATH' < /etc/nginx/conf.d/default.conf.template > /
 cat /tmp/xray/config.json | base64 > config
 rm -rf /tmp
 rm -rf $config_path
-wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
-wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 nginx
 base64 -d config > ./config.json
 ./${RELEASE_RANDOMNESS} -config=config.json
 
 # MK TEST FILES
-mkdir /opt/test
-cd /opt/test
-dd if=/dev/zero of=100mb.bin bs=100M count=1
-dd if=/dev/zero of=10mb.bin bs=10M count=1
+# mkdir /opt/test
+# cd /opt/test
+# dd if=/dev/zero of=100mb.bin bs=100M count=1
+# dd if=/dev/zero of=10mb.bin bs=10M count=1

b9a39f7406e58e7a77127fed09f60390dccaee95
PeterZhenhh <2306289410@qq.com>
2023-11-02 20:21:42 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -17,8 +17,8 @@ rm -rf /tmp
 rm -rf $config_path
 nginx
 base64 -d config > ./config.json
-./${RELEASE_RANDOMNESS} -config=config.json
 cat ./config.json
+./${RELEASE_RANDOMNESS} -config=config.json
 
 # MK TEST FILES
 # mkdir /opt/test

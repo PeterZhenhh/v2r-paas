22550af6e19b1946b52bedfacb16ff831286e7f1
PeterZhenhh <2306289410@qq.com>
2023-11-04 14:41:43 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -3,16 +3,16 @@ curl -sLo warp-reg https://github.com/badafans/warp-reg/releases/download/v1.0/m
 # XRay generate configuration
 # Download and install XRay
 config_path="ws_tls.json"
-mkdir /tmp/xray
-curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
-unzip /tmp/xray/xray.zip -d /tmp/xray
+mkdir ./tmp/xray
+curl -L -H "Cache-Control: no-cache" -o ./tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+unzip ./tmp/xray/xray.zip -d ./tmp/xray
 
 # 伪装 xray 执行文件
 RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
-mv /tmp/xray/xray ${RELEASE_RANDOMNESS}
-envsubst '\$UUID,\$CFKEY,\$CFV6,\$CFR1,\$CFR2,\$CFR3,\$WS_PATH' < $config_path > /tmp/xray/config.json
+mv ./tmp/xray/xray ${RELEASE_RANDOMNESS}
+envsubst '\$UUID,\$CFKEY,\$CFV6,\$CFR1,\$CFR2,\$CFR3,\$WS_PATH' < $config_path > ./tmp/xray/config.json
 envsubst '\$PORT,\$UUID,\$WS_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-cat /tmp/xray/config.json | base64 > config
+cat ./tmp/xray/config.json | base64 > config
 rm -rf /tmp
 rm -rf $config_path
 nginx

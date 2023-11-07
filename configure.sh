d7de11d04e97c1827d4f1b5b61f8157b3676e50f
PeterZhenhh <2306289410@qq.com>
2023-11-07 14:01:28 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,29 +1,32 @@
 #!/bin/sh
+# Cloudflare Warp
 curl -sLo warp-reg https://github.com/badafans/warp-reg/releases/download/v1.0/main-linux-amd64 && chmod +x warp-reg && ./warp-reg
-# XRay generate configuration
-# Download and install XRay
-config_path="ws_tls.json"
-mkdir ./tmp/xray
-curl -L -H "Cache-Control: no-cache" -o ./tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
-unzip ./tmp/xray/xray.zip -d ./tmp/xray
 
-# 伪装 xray 执行文件
-RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
-mv ./tmp/xray/xray ${RELEASE_RANDOMNESS}
-envsubst '\$UUID,\$CFKEY,\$CFV6,\$CFR1,\$CFR2,\$CFR3,\$WS_PATH' <$config_path >./tmp/xray/config.json
-envsubst '\$PORT,\$UUID,\$WS_PATH' </etc/nginx/conf.d/default.conf.template >/etc/nginx/conf.d/default.conf
-cat ./tmp/xray/config.json | base64 >config
-rm -rf ./tmp
-rm -rf $config_path
-nginx
-base64 -d config >./config.json
-./${RELEASE_RANDOMNESS} -config=config.json &
-
-# tailscale
-if [ -z $TAILSCALE_HOSTNAME ]; then
-    TAILSCALE_HOSTNAME=`hostname`
-    echo "【TAILSCALE】 TAILSCALE_HOSTNAME not configured, using $TAILSCALE_HOSTNAME"
+# Xray
+if [ -z $UUID ]; then
+    echo "xray UUID未配置"
+else
+    config_path="ws_tls.json"
+    mkdir -p ./tmp/xray
+    # 下载xray
+    wget -O ./tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+    unzip ./tmp/xray/xray.zip -d ./tmp/xray
+    # 伪装 xray 执行文件
+    RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
+    mv ./tmp/xray/xray ${RELEASE_RANDOMNESS}
+    envsubst '\$UUID,\$CFKEY,\$CFV6,\$CFR1,\$CFR2,\$CFR3,\$WS_PATH' <$config_path >./tmp/xray/config.json
+    envsubst '\$PORT,\$UUID,\$WS_PATH' </etc/nginx/conf.d/default.conf.template >/etc/nginx/conf.d/default.conf
+    cat ./tmp/xray/config.json | base64 >config
+    # 启动xray
+    base64 -d config >./config.json
+    ./${RELEASE_RANDOMNESS} -config=config.json &
+    # 清理
+    rm -rf ./tmp
+    rm -rf $config_path
 fi
+
+# Tailscale
+TAILSCALE_HOSTNAME=${TAILSCALE_HOSTNAME:-$(hostname)}
 if [ -z $TAILSCALE_AUTHKEY ]; then
     echo "【TAILSCALE】 TAILSCALE_AUTHKEY not configured"
 else
@@ -33,4 +36,5 @@ else
     /app/tailscale up --authkey=$TAILSCALE_AUTHKEY --hostname=$TAILSCALE_HOSTNAME --advertise-exit-node &
 fi
 
-./${RELEASE_RANDOMNESS} -config=config.json
+# Nginx
+nginx
